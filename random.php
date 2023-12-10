267f479eab98b5365554f2dbcc1720277c1c0617
jjsal1234 <143920177+jjsal1234@users.noreply.github.com>
2023-12-09 22:58:25 -0700

    Create random.php

---
+++ random.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random PHP File</title>
+</head>
+<body>
+
+<h1>Random Number Generator</h1>
+
+<?php
+// Generate a random number between 1 and 10
+$randomNumber = rand(1, 10);
+
+// Display the random number
+echo "<p>Random Number: $randomNumber</p>";
+?>
+
+</body>
+</html>
